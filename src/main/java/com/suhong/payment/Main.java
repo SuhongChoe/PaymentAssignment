800e8b038a003db4af56f790ed15d7e33444e197
choesuhong <csoohong64@gmail.com>
2026-07-27 22:25:27 +0900

    refactor: remove obsolete payment code

--- src/main/java/com/suhong/payment/Main.java
+++ src/main/java/com/suhong/payment/Main.java
@@ -6,53 +6,16 @@ public class Main {
     public static void main(String[] args) {
         int amount = 15000;
 
-        // 1차 작성 코드 -> 단순히 같은 변수에 다른 객체를 넣으려는 목적
-//        // 신용카드 결제
-//        PaymentMethod paymentMethod = new CardPayment();
-//        paymentMethod.pay(amount);
-//
-//        // 카카오페이 결제
-//        paymentMethod = new KakaoPayPayment();
-//        paymentMethod.pay(amount);
-//
-//        // 계좌이체 결제
-//        paymentMethod = new BankTransferPayment();
-//        paymentMethod.pay(amount);
-
-        // OrderService를 사용하여 결제 수단별로 서비스를 생성
-
-        /*
-        // 신용카드 결제
-        PaymentMethod cardPayment = new CardPayment();
-        OrderService cardOrderService = new OrderService(cardPayment);
-        cardOrderService.checkout(amount);
-
-        // 카카오페이 결제
-        PaymentMethod kakaoPayPayment = new KakaoPayPayment();
-        OrderService kakaoOrderService = new OrderService(kakaoPayPayment);
-        kakaoOrderService.checkout(amount);
-
-        // 계좌이체 결제
-        PaymentMethod bankTransferPayment = new BankTransferPayment();
-        OrderService bankTransferOrderService = new OrderService(bankTransferPayment);
-        bankTransferOrderService.checkout(amount);
-
-        // 포인트 결제
-        PaymentMethod pointPayment = new PointPayment();
-        OrderService pointOrderService = new OrderService(pointPayment);
-        pointOrderService.checkout(amount);
-        */
-
-        List<PaymentMethod> paymentMethodList = List.of(
+        List<PaymentMethod> paymentMethods = List.of(
                 new CardPayment(),
                 new KakaoPayPayment(),
                 new BankTransferPayment(),
                 new PointPayment()
         );
 
-        for (PaymentMethod paymentMethod : paymentMethodList) {
-            OrderService paymentOrderService = new OrderService(paymentMethod);
-            paymentOrderService.checkout(amount);
+        for (PaymentMethod paymentMethod : paymentMethods) {
+            OrderService orderService = new OrderService(paymentMethod);
+            orderService.checkout(amount);
         }
     }
 }
